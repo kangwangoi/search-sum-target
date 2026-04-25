b7ab3fcb28cd125eb2f372c6e77665dd11a2f8c5
kangwangoi <153366806+kangwangoi@users.noreply.github.com>
2026-04-25 13:58:51 +0700

    js

---
+++ searchSum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let i = 0
+    for(n=1; n<=nums.length; n++){
+     if(i<= nums.length){
+        if(nums[i]+nums[n] == target){
+            return [i, n];
+            break;
+        } else{
+            if(n>=nums.length){
+                n = 1
+                i++
+            }
+        }
+    }
+    }
+};
